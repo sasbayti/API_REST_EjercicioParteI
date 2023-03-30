4ebfeb3968b102c7dcfb191ac6cf5a18a8a3536b
Salma <salma.asbayti@gmail.com>
2023-03-30 14:33:04 +0200

    Estado inicial

---
+++ src/main/java/com/example/ApiRestEjercicioParteIApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestEjercicioParteIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestEjercicioParteIApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/entities/Cliente.java
@@ -0,0 +1,32 @@
+package com.example.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Cliente implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    
+    @NotEmpty(message = "El nombre no puede estar vacío")
+    @Size(min = 4, max = 25, message = "El nombre tiene que estar entre 4 y 25 caracteres")
+    private String nombre;
+    
+}
